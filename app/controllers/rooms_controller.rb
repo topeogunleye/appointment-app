2bc81330b69e77331470646892d0c9f1830312c0
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-11-13 08:19:29 +0100

    Add show action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,16 @@
 class RoomsController < ApplicationController
+  
   def index
     @rooms = Room.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
   end
 end
